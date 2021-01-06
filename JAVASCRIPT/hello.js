6c97437e5e42c1f0d7cb00490e28085bf17199a5
viralsir <viralsir2018@gmail.com>
2021-01-06 10:32:33 +0530

    javascript introduction and queryselector

---
+++ JAVASCRIPT/hello.js
@@ -0,0 +1,17 @@
+ function hello()
+ {
+
+            let h1tag=document.querySelector("h1");
+
+            if(h1tag.innerHTML=="hello")
+            {
+                h1tag.innerHTML="Good bye";
+            }
+            else{
+                h1tag.innerHTML="hello";
+            }
+
+        }
+        document.addEventListener('DOMContentLoaded',function(){
+            document.querySelector("button").onclick=hello;
+        })
